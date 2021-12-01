f3cb3682962e986afa41f2474c9bff13c0cc77f5
Matthew McGrath <rubywep@hotmail.com>
2021-11-30 21:37:03 -0800

    Begin Section 28 - React Router, and complete notes as well as progress on a small app, up until Lecture 280. The project is started in an older version of React Router, which I will update after the section ends.

---
+++ Section 20 - React Router/09-time-to-practice-starting-code/src/pages/NewQuote.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewQuote() {
+  return (
+    <div>
+      <h1>New Quote</h1>
+    </div>
+  );
+}
+
+export default NewQuote;
---
+++ Section 20 - React Router/09-time-to-practice-starting-code/src/pages/QuoteDetail.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+function QuoteDetail() {
+  const params = useParams();
+
+  return (
+    <React.Fragment>
+      <h1>Quote Detail</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </React.Fragment>
+  );
+}
+
+export default QuoteDetail;
